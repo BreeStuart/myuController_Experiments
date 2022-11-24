f66eb83d8de4caca84997103051a721e83fb6b45
Ashley <ashleystuart@outlook.com.au>
2022-11-24 16:31:11 +1100

    Initial Commit of Hello Again Project.

---
+++ ESP32 Hello Again/src/secrets.h
@@ -0,0 +1,7 @@
+const char *WiFi_ssid = "KMT-IOT";
+const char *WiFi_psk = "V'>FdH/vS.DNh6%^";
+const char *MQTT_addr = "192.168.42.9";
+const int MQTT_port = 1883;
+const char *MQTT_user = "esp32dev";
+const char *MQTT_pass = "habvi;wdbhs";
+const char *MQTT_LED_topic = "/test/led/state";
